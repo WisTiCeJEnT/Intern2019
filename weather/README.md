b763d4a402dce5d4720ed4e4379146977b4e96ac
Tanakorn Prangsriarun <tanagon601@gmail.com>
2019-01-31 23:46:16 +0700

    weather done

---
+++ weather/README.md
@@ -0,0 +1,11 @@
+# XML2JSON
+
+# Using
+
+#type python3 xml2json yourxmlfile.xml__
+make sure you type correctly.__
+
+#Reference
+Using import json for saving dictionary in python to json file__
+https://pythonspot.com/save-a-dictionary-to-a-file/ __
+
--- weather/xml2json.py
+++ weather/xml2json.py
@@ -1,3 +1,5 @@
+import sys
+import json
 def extract(s):
     InTag=s[s.find('<'):s.find('>')+1].strip('</>').split('"')[:-1]
     OutTag=s[1:][s[1:].find('>')+1:s[1:].find('<')]
@@ -23,72 +25,81 @@ def extract(s):
     return res
    
 
+file=(sys.argv[1:])[0]
+if '.xml' in file:
+    line=open(file,'r')
+    text=''
+    for i in line:
+        text+=i.strip()
+    stack=[]
+    tmp=text[0]
+    for i in range(1,len(text)-1):
+        tmp+=text[i]
+        if text[i]=='>' and text[i+1]=='<':
+            stack.append(tmp)
+            tmp=''
+        if i==len(text)-2:
+            tmp+=text[len(text)-1]
+            stack.append(tmp)
 
-line=open('read.xml','r')
-text=''
-for i in line:
-    text+=i.strip()
-stack=[]
-tmp=text[0]
-for i in range(1,len(text)-1):
-    tmp+=text[i]
-    if text[i]=='>' and text[i+1]=='<':
-        stack.append(tmp)
-        tmp=''
-    if i==len(text)-2:
-        tmp+=text[len(text)-1]
-        stack.append(tmp)
-
-stack=stack[1:len(stack)-1]
-sep=[]
-tmp=[]
-i=0
-while i<len(stack):
-    s=stack[i]
-    
-    t=s[s.find('<')+1:s.find(' ')+1]
-    
-    if (t ==''):
+    stack=stack[1:len(stack)-1]
+    sep=[]
+    tmp=[]
+    i=0
+    while i<len(stack):
+        s=stack[i]
         
-        end=stack.index('<'+'/'+s.strip('<>')+'>')
-        sep.append(stack[i:end+1])
-        i=end+1
-        continue
-    if (('<'+'/'+t.strip()+'>') in stack):
-        end=stack.index('<'+'/'+t.strip()+'>') 
-        sep.append(stack[i:end+1])
-        i=end+1
-        continue
-    else:
-        sep.append([stack[i]])
-        i+=1
-    
-for i in sep:
-    print(i)
-res={}
-for i in range(len(sep)):
-    get=extract(sep[i][0])
-    if len(get)!=1:
-        k=get[0]
-        v=get[1]
-        n=len(sep[i])
-        for j in range(len(sep[i])-2):
-            inget=extract(((sep[i])[1:n-1])[j])
-            ink=inget[0]
-            inv=inget[1]
-            v[ink]=inv
-        res[k]=v
-    else:
-        k=get[0]
-        v={}
-        n=len(sep[i])
-        for j in range(len(sep[i])-2):
-            inget=extract(((sep[i])[1:n-1])[j])
-            ink=inget[0]
-            inv=inget[1]
-            v[ink]=inv
-        res[k]=v
+        t=s[s.find('<')+1:s.find(' ')+1]
+        
+        if (t ==''):
+            
+            end=stack.index('<'+'/'+s.strip('<>')+'>')
+            sep.append(stack[i:end+1])
+            i=end+1
+            continue
+        if (('<'+'/'+t.strip()+'>') in stack):
+            end=stack.index('<'+'/'+t.strip()+'>') 
+            sep.append(stack[i:end+1])
+            i=end+1
+            continue
+        else:
+            sep.append([stack[i]])
+            i+=1
+        
+    res={}
+    for i in range(len(sep)):
+        get=extract(sep[i][0])
+        if len(get)!=1:
+            k=get[0]
+            v=get[1]
+            n=len(sep[i])
+            for j in range(len(sep[i])-2):
+                inget=extract(((sep[i])[1:n-1])[j])
+                ink=inget[0]
+                inv=inget[1]
+                v[ink]=inv
+            res[k]=v
+        else:
+            k=get[0]
+            v={}
+            n=len(sep[i])
+            for j in range(len(sep[i])-2):
+                inget=extract(((sep[i])[1:n-1])[j])
+                ink=inget[0]
+                inv=inget[1]
+                v[ink]=inv
+            res[k]=v
 
+        
     
-
+    json = json.dumps(res,indent=4)
+    f = open(file.strip('.xml')+".json","w")
+    f.write(json)
+    f.close()
+    print('Genterated:                '+file.strip('.xml')+'.json')
+    print('Read json by typing:       python3 xml2json '+file.strip('.xml')+'.json')
+else:
+    line=open(file,"r")
+    for i in line:
+        print(i)
 
